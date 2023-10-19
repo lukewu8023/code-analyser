8841b63eed354f8b564668ece68d25b0eb574b2a
lukewu8023 <luke8023@gmail.com>
2023-10-20 02:42:25 +0800

    update waeviate_connection with key_config

--- waeviate_connection.py
+++ waeviate_connection.py
@@ -1,11 +1,10 @@
 import os
 import weaviate
+import key_config
 
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Weaviate
 
-os.environ["OPENAI_API_BASE"] = "https://api.ohmygpt.com/v1/"
-os.environ["OPENAI_API_KEY"] = ""
 EMBEDDING_MODEL = "text-embedding-ada-002"
 
 embeddings = OpenAIEmbeddings()
@@ -15,12 +14,14 @@ query_result = embeddings.embed_query(text)
 print(f"+++Text embedding length: {len(query_result)}")
 
 client = weaviate.Client(
-    url="",
+    url=os.environ.get("WEAVIATE_URL"),
     additional_headers={"X-OpenAI-Api-Key": os.getenv("OPENAI_API_KEY")},
 )
 print(f"+++Weaviate is ready? {client.is_ready()}")
 
-db = Weaviate.from_texts(text, embeddings, weaviate_url="", by_text=False)
+db = Weaviate.from_texts(
+    text, embeddings, weaviate_url=os.environ.get("WEAVIATE_URL"), by_text=False
+)
 
 query = "what do you like?"
 docs = db.similarity_search(query)
