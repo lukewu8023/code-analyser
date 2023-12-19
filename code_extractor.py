063117e72153ed36c15fd5f97916ca3f1814c2b2
lukewu8023 <luke8023@gmail.com>
2023-12-19 18:01:42 +0800

    enhance code extractor

--- code_extractor.py
+++ code_extractor.py
@@ -10,7 +10,7 @@ from langchain.document_loaders.parsers import LanguageParser
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 loader = GenericLoader.from_filesystem(
-    "/Users/luke/Documents/workspace/java/code-understanding/src/main/java/com/example/demo/service",
+    "/Users/luke/Documents/workspace/java/code-understanding",
     glob="**/*",
     suffixes=[".java"],
     parser=LanguageParser(),
@@ -24,15 +24,15 @@ print(f"+++Number of loaded documents: {len(documents)}")
 # for text in texts:
 #     print(f"+++Trunk text content: {text}")
 
-chat = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, max_tokens=1024)
+chat = ChatOpenAI(model="gpt-3.5-turbo-0613", temperature=0, max_tokens=1024)
 
-with open("prompt_extraction.txt") as f:
+with open("prompt/prompt_extraction.txt") as f:
     contents = f.read()
 template_string = contents
 prompt_template = ChatPromptTemplate.from_template(template_string)
 
 code = ""
-with open("source_code_uc.json", "a") as file:
+with open("data/code.json", "a") as file:
     for document in documents:
         name = document.metadata["source"]
         code = document.page_content
