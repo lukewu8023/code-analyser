40faedd8bc552df7613cc1c5699b27db13ec5256
lukewu8023 <luke8023@gmail.com>
2023-12-19 22:47:46 +0800

    code extraction Codev1

--- load_data.py
+++ load_data.py
@@ -3,8 +3,6 @@ import os
 import weaviate
 import key_config
 
-from langchain import PromptTemplate
-from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
 
 with open("data/code_cu.json") as f:
@@ -14,15 +12,18 @@ data = json.loads(contents)
 # data = json.load(f)
 print(f"+++Data: {data}")
 
-for i, method in enumerate(data["methods"]):
-    print(f"+++method: {i+1}")
-    print(method["method_name"])
+# [test file load]
+# for i, method in enumerate(data["methods"]):
+#     print(f"+++method: {i+1}")
+#     print(method["method_name"])
 
 embeddings_model = OpenAIEmbeddings()
-embeddings = embeddings_model.embed_documents(["Hi there!"])
-print(len(embeddings[0]))
+# [test embedding]
+# embeddings = embeddings_model.embed_documents(["Hi there!"])
+# print(len(embeddings[0]))
 
 client = weaviate.Client(os.environ.get("WEAVIATE_URL"))
+print(f"+++Weaviate is ready? {client.is_ready()}")
 
 client.batch.configure(batch_size=100)
 with client.batch as batch:
@@ -33,23 +34,34 @@ with client.batch as batch:
             "method_name": method["method_name"],
             "package_name": method["package_name"],
             "class_name": method["class_name"],
+            "is_interface": method["is_interface"],
+            "parameters": method["parameters"],
+            "invoked_methods": method["invoked_methods"],
             "method_desc": method["method_desc"],
         }
 
         embed_document = (
-            "method name is "
+            "method name is ["
             + method["method_name"]
-            + ", package name is "
+            + "], package name is ["
             + method["package_name"]
-            + ", class name is "
+            + "], class name is ["
             + method["class_name"]
-            + ", method description is "
+            + "], this method "
+            + ("" if method["is_interface"] else "not")
+            + " belongs to an interface"
+            + ", parameters in this method are "
+            + json.dumps(method["parameters"])
+            + ", invoked methods in this method are "
+            + json.dumps(method["invoked_methods"])
+            + ", method description is ["
             + method["method_desc"]
+            + "]"
         )
         print(f"+++embed document: {embed_document}")
 
-        embeddings = embeddings_model.embed_documents([method["method_desc"]])
+        embeddings = embeddings_model.embed_documents(embed_document)
         print(len(embeddings[0]))
         # print(embeddings[0])
 
-        batch.add_data_object(properties, "Code1", vector=embeddings[0])
+        batch.add_data_object(properties, "Codev1", vector=embeddings[0])
--- manage_schema.py
+++ manage_schema.py
@@ -5,8 +5,8 @@ import key_config
 
 client = weaviate.Client(os.environ.get("WEAVIATE_URL"))
 
-# client.schema.delete_class("LangChain_10b4617a9f7e4626ad403e60e8843ed5")
-class_name = "Code1"
+client.schema.delete_class("Codev1")
+class_name = "Codev1"
 
 new_class_obj = {
     "class": class_name,
@@ -14,11 +14,29 @@ new_class_obj = {
         {"name": "package_name", "dataType": ["text"]},
         {"name": "class_name", "dataType": ["text"]},
         {"name": "method_name", "dataType": ["text"]},
+        {"name": "is_interface", "dataType": ["boolean"]},
+        {
+            "name": "parameters",
+            "dataType": ["object[]"],
+            "nestedProperties": [
+                {"dataType": ["text"], "name": "name"},
+                {"dataType": ["text"], "name": "type"},
+            ],
+        },
+        {
+            "name": "invoked_methods",
+            "dataType": ["object[]"],
+            "nestedProperties": [
+                {"dataType": ["text"], "name": "variable_class_name"},
+                {"dataType": ["text"], "name": "variable_name"},
+                {"dataType": ["text"], "name": "method_name"},
+            ],
+        },
         {"name": "method_desc", "dataType": ["text"]},
     ],
     "vectorizer": "none",
 }
-# client.schema.create_class(new_class_obj)
+client.schema.create_class(new_class_obj)
 
 response = client.schema.get(class_name)
 # response = client.schema.get()
